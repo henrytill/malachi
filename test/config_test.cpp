230cc318536d13adfd5a4b8e0c73903bceffdf35
Henry Till <henrytill@gmail.com>
2023-12-19 21:22:27 -0800

    add initial work

---
+++ test/config_test.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "Platform.h"
+
+namespace config = malachi::config;
+
+TEST_CASE("to_string returns the correct string for each platform", "[Platform]") {
+  using malachi::config::Platform;
+
+  REQUIRE(config::to_string(Platform::Windows) == "Windows");
+  REQUIRE(config::to_string(Platform::MacOS) == "MacOS");
+  REQUIRE(config::to_string(Platform::Linux) == "Linux");
+  REQUIRE(config::to_string(Platform::Unknown) == "Unknown");
+}
+
+int main(int argc, char *argv[]) {
+  int result = Catch::Session().run(argc, argv);
+  return result;
+}
