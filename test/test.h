6610b67f8085c27469aff4d71abc0ed2173bca6e
Henry Till <henrytill@gmail.com>
2023-12-25 18:51:13 -0800

    test.h: add line number to TAG

--- test/test.h
+++ test/test.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAG(s)       printf("%s:%s: %s\n", __FILE__, __func__, (s))
+#define TAG(s)       printf("%s:%d:%s: %s\n", __FILE__, __LINE__, __func__, (s))
 #define BEGIN_TEST() TAG("Starting...")
 #define END_TEST()   TAG("Passed!")
 
