bba36b75e9e80ad48d70ab4d14f93ff9c9508efe
Henry Till <henrytill@gmail.com>
2024-03-24 14:02:53 -0700

    restructure build

---
+++ GNUmakefile
@@ -0,0 +1,40 @@
+CFLAGS = -g -std=c11 -Iinclude -Wall -Wextra -Wconversion -Wsign-conversion
+LDLIBS = -lsqlite3 -lmupdf
+BINOUT = _bin
+
+HEADERS =
+HEADERS += include/config.h
+HEADERS += include/path.h
+HEADERS += include/platform.h
+
+OBJECTS =
+OBJECTS += src/config.o
+OBJECTS += src/main.o
+OBJECTS += src/path.o
+
+TEST_OBJECTS =
+TEST_OBJECTS += src/config.o
+TEST_OBJECTS += src/path.o
+TEST_OBJECTS += test/config_test.o
+
+.PHONY: all
+all: $(BINOUT)/malachi $(BINOUT)/config_test
+
+$(OBJECTS): $(HEADERS)
+
+$(BINOUT):
+	mkdir -p -- $(BINOUT)
+
+$(BINOUT)/malachi: $(OBJECTS) $(BINOUT)
+	$(CC) $(LDFLAGS) -o $@ $(OBJECTS) $(LDLIBS)
+
+$(BINOUT)/config_test: $(TEST_OBJECTS) $(BINOUT)
+	$(CC) $(LDFLAGS) -o $@ $(TEST_OBJECTS) $(LDLIBS)
+
+.PHONY: check
+check: $(BINOUT)/config_test
+	$(BINOUT)/config_test
+
+.PHONY: clean
+clean:
+	rm -rf -- $(BINOUT) $(OBJECTS) $(TEST_OBJECTS)
--- build.zig
+++ build.zig
@@ -4,21 +4,19 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const platform_include = "include/platform";
+    const include = "include";
 
     const path_lib = b.addStaticLibrary(.{
         .name = "libpath",
         .target = target,
         .optimize = optimize,
     });
-    const path_lib_include = "include/path";
     const path_lib_sources = &.{
-        "lib/path/path.c",
+        "src/path.c",
     };
 
     path_lib.linkLibC();
-    path_lib.addIncludePath(.{ .path = path_lib_include });
-    path_lib.addIncludePath(.{ .path = platform_include });
+    path_lib.addIncludePath(.{ .path = include });
     if (@hasDecl(std.Build.Step.Compile, "AddCSourceFilesOptions")) {
         path_lib.addCSourceFiles(.{ .files = path_lib_sources });
     } else {
@@ -30,16 +28,13 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    const config_lib_include = "include/config";
     const config_lib_sources = &.{
-        "lib/config/config.c",
+        "src/config.c",
     };
 
     config_lib.linkLibC();
     config_lib.linkLibrary(path_lib);
-    config_lib.addIncludePath(.{ .path = config_lib_include });
-    config_lib.addIncludePath(.{ .path = path_lib_include });
-    config_lib.addIncludePath(.{ .path = platform_include });
+    config_lib.addIncludePath(.{ .path = include });
     if (@hasDecl(std.Build.Step.Compile, "AddCSourceFilesOptions")) {
         config_lib.addCSourceFiles(.{ .files = config_lib_sources });
     } else {
@@ -48,7 +43,7 @@ pub fn build(b: *std.Build) void {
 
     const main_exe = b.addExecutable(.{
         .name = "malachi",
-        .root_source_file = .{ .path = "bin/main.c" },
+        .root_source_file = .{ .path = "src/main.c" },
         .target = target,
         .optimize = optimize,
     });
@@ -57,8 +52,7 @@ pub fn build(b: *std.Build) void {
     main_exe.linkSystemLibrary("mupdf");
     main_exe.linkSystemLibrary("sqlite3");
     main_exe.linkLibrary(config_lib);
-    main_exe.addIncludePath(.{ .path = config_lib_include });
-    main_exe.addIncludePath(.{ .path = platform_include });
+    main_exe.addIncludePath(.{ .path = include });
     b.installArtifact(main_exe);
 
     const config_test_exe = b.addExecutable(.{
@@ -70,8 +64,7 @@ pub fn build(b: *std.Build) void {
 
     config_test_exe.linkLibC();
     config_test_exe.linkLibrary(config_lib);
-    config_test_exe.addIncludePath(.{ .path = config_lib_include });
-    config_test_exe.addIncludePath(.{ .path = platform_include });
+    config_test_exe.addIncludePath(.{ .path = include });
     b.installArtifact(config_test_exe);
 
     const run_config_test = b.addRunArtifact(config_test_exe);
