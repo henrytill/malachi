a1bdede471c1fd2e05d28bacb3af8a206b91f3ba
Henry Till <henrytill@gmail.com>
2024-03-24 12:57:40 -0700

    path: update calloc sizes

--- lib/path/path.c
+++ lib/path/path.c
@@ -15,7 +15,7 @@ char *joinpath2(const char *a, const char *b)
 {
     extern const char SEPARATOR;
     size_t len = (size_t)snprintf(NULL, 0, "%s%c%s", a, SEPARATOR, b);
-    char *ret = calloc(++len, sizeof(char)); // incr for terminator
+    char *ret = calloc(++len, sizeof(*ret)); // incr for terminator
     if (ret == NULL) { return NULL; }
     (void)snprintf(ret, len, "%s%c%s", a, SEPARATOR, b);
     return ret;
@@ -25,7 +25,7 @@ char *joinpath3(const char *a, const char *b, const char *c)
 {
     extern const char SEPARATOR;
     size_t len = (size_t)snprintf(NULL, 0, "%s%c%s%c%s", a, SEPARATOR, b, SEPARATOR, c);
-    char *ret = calloc(++len, sizeof(char)); // incr for terminator
+    char *ret = calloc(++len, sizeof(*ret)); // incr for terminator
     if (ret == NULL) { return NULL; }
     (void)snprintf(ret, len, "%s%c%s%c%s", a, SEPARATOR, b, SEPARATOR, c);
     return ret;
@@ -35,7 +35,7 @@ char *joinpath4(const char *a, const char *b, const char *c, const char *d)
 {
     extern const char SEPARATOR;
     size_t len = (size_t)snprintf(NULL, 0, "%s%c%s%c%s%c%s", a, SEPARATOR, b, SEPARATOR, c, SEPARATOR, d);
-    char *ret = calloc(++len, sizeof(char)); // incr for terminator
+    char *ret = calloc(++len, sizeof(*ret)); // incr for terminator
     if (ret == NULL) { return NULL; }
     (void)snprintf(ret, len, "%s%c%s%c%s%c%s", a, SEPARATOR, b, SEPARATOR, c, SEPARATOR, d);
     return ret;
