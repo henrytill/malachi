5058505b0ed7a4b2a3c087b9f7109f45002c27e7
Henry Till <henrytill@gmail.com>
2024-10-12 02:41:18 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.opam-repository.follows = "opam-repository";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      follows = "opam-nix/flake-utils";
+    };
+  };
+  outputs =
+    {
+      self,
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      package = "malachi";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = "5.2.0"; };
+        overlay = final: prev: {
+          ${package} = prev.${package}.overrideAttrs (as: {
+            buildInputs = as.buildInputs ++ (with pkgs; [ (poppler.overrideAttrs { suffix = "cpp"; }) ]);
+          });
+        };
+      in
+      {
+        legacyPackages = scope.overrideScope' overlay;
+        packages.default = self.legacyPackages.${system}.${package};
+      }
+    );
+}
