d320ddb27958dedbfbf4e1c5876d57687bdf14b7
Henry Till <henrytill@gmail.com>
2024-10-12 02:52:59 -0700

    switch from poppler to mupdf

--- config/discover.ml
+++ config/discover.ml
@@ -2,8 +2,8 @@ module C = Configurator.V1
 
 let call_pkg_config pc =
   let open C.Pkg_config in
-  let package = "poppler-cpp" in
-  let expr = "poppler-cpp >= 22.02.0" in
+  let package = "mupdf" in
+  let expr = "mupdf >= 1.19.0" in
   match query_expr_err pc ~package ~expr with
   | Ok c -> c
   | Error err -> C.die "%s" err
@@ -12,10 +12,10 @@ let configure c =
   let open C.Pkg_config in
   let { cflags; libs } =
     match get c with
-    | None -> { libs = [ "-lpoppler-cpp" ]; cflags = [] }
+    | None -> { libs = [ "-lmupdf" ]; cflags = [] }
     | Some pc -> call_pkg_config pc
   in
-  C.Flags.write_sexp "cflags.sexp" cflags;
-  C.Flags.write_sexp "libs.sexp" libs
+  C.Flags.write_sexp "mupdf_cflags.sexp" cflags;
+  C.Flags.write_sexp "mupdf_libs.sexp" libs
 
 let () = C.main ~name:"malachi" configure
---
+++ lib/mupdf_stubs.c
@@ -0,0 +1,10 @@
+#include <caml/alloc.h>
+#include <caml/memory.h>
+#include <mupdf/fitz.h>
+
+CAMLprim value caml_mupdf_version_string(value unit) {
+  CAMLparam1(unit);
+  CAMLlocal1(caml_result);
+  caml_result = caml_copy_string(FZ_VERSION);
+  CAMLreturn(caml_result);
+}
