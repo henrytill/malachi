a53cdac28de89df2ea648875c06aad47420cd1d8
Henry Till <henrytill@gmail.com>
2024-12-11 13:28:28 -0800

    update .clang-format and reformat

--- lib/mupdf_stubs.c
+++ lib/mupdf_stubs.c
@@ -2,10 +2,9 @@
 #include <caml/memory.h>
 #include <mupdf/fitz.h>
 
-CAMLprim value caml_mupdf_version_string(value unit)
-{
-    CAMLparam1(unit);
-    CAMLlocal1(caml_result);
-    caml_result = caml_copy_string(FZ_VERSION);
-    CAMLreturn(caml_result);
+CAMLprim value caml_mupdf_version_string(value unit) {
+  CAMLparam1(unit);
+  CAMLlocal1(caml_result);
+  caml_result = caml_copy_string(FZ_VERSION);
+  CAMLreturn(caml_result);
 }
