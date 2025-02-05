117e9e783c3185c7fcf7345629c60613d81da8de
Henry Till <henrytill@gmail.com>
2025-02-05 00:05:55 -0800

    Environment::get_env: tweak

--- test/environment.h
+++ test/environment.h
@@ -9,7 +9,7 @@ public:
   static constexpr auto getenv(const std::string_view name) -> char * {
     const auto &vars = Derived::env;
     const auto *const iterator = std::ranges::find_if(vars, [name](const auto &pair) {
-      return pair.first == name;
+      return name == pair.first;
     });
     if (iterator != vars.end()) {
       return const_cast<char *>(iterator->second);
