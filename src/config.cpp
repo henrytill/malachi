f5cbc63b8501af4eb9b466f613fac22195f5c192
Henry Till <henrytill@gmail.com>
2025-02-27 18:08:40 -0800

    config: reposition Config::{platform,to_string}

--- src/config.cpp
+++ src/config.cpp
@@ -12,6 +12,17 @@ static const auto kName = std::filesystem::path{"malachi"};
 static constexpr auto kMissingConfigDirMsg = std::string_view{"Configuration directory could not be determined"};
 static constexpr auto kMissingDataDirMsg = std::string_view{"Data directory could not be determined"};
 
+auto Config::platform() -> platform::Platform {
+  return platform::get_platform();
+}
+
+auto Config::to_string() const -> std::string {
+  return std::format("platform: {}\nconfig_dir: {}\ndata_dir: {}\n",
+                     platform::to_string_view(platform()),
+                     config_dir.string(),
+                     data_dir.string());
+}
+
 Builder::Builder(platform::GetEnvFn getenv) : getenv_{std::move(getenv)} {}
 
 auto Builder::with_defaults() && -> Builder && {
@@ -36,17 +47,6 @@ auto Builder::build() && -> Result {
   return Config{
       .config_dir = std::move(maybe_config_dir_.value()),
       .data_dir = std::move(maybe_data_dir_.value())};
-}
-
-auto Config::platform() -> platform::Platform {
-  return platform::get_platform();
-}
-
-auto Config::to_string() const -> std::string {
-  return std::format("platform: {}\nconfig_dir: {}\ndata_dir: {}\n",
-                     platform::to_string_view(platform()),
-                     config_dir.string(),
-                     data_dir.string());
 }
 
 } // namespace malachi::config
