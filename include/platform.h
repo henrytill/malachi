09dbaab25dc576e8293f6da995c1b0c8af5667e2
Henry Till <henrytill@gmail.com>
2025-08-15 22:29:17 -0700

    platform: add aliases for {optional,path}, tweak formatting

--- include/platform.h
+++ include/platform.h
@@ -10,6 +10,11 @@
 
 namespace platform {
 
+template <typename T>
+using optional = std::optional<T>;
+
+using path = std::filesystem::path;
+
 using GetEnvFn = std::function<char *(char const *)>;
 
 enum class Platform : uint8_t {
@@ -29,7 +34,8 @@ enum class Platform : uint8_t {
 [[nodiscard]] constexpr auto get_platform() -> Platform { return Platform::Unknown; }
 #endif
 
-[[nodiscard]] constexpr auto to_string_view(const Platform platform) -> std::string_view {
+[[nodiscard]]
+constexpr auto to_string_view(const Platform platform) -> std::string_view {
     switch (platform) {
     case Platform::Windows:
         return "Windows";
@@ -46,22 +52,22 @@ enum class Platform : uint8_t {
 
 namespace windows {
 
-[[nodiscard]] inline auto get_app_data(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+inline auto get_app_data(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     auto app_data = std::unique_ptr<char>{getenv("APPDATA")};
     if (app_data != nullptr) {
-        auto const config_dir = std::filesystem::path{app_data.release()};
-        return std::optional<std::filesystem::path>{config_dir / name};
+        auto const config_dir = path{app_data.release()};
+        return optional<path>{config_dir / name};
     }
     return std::nullopt;
 }
 
-[[nodiscard]] inline auto get_local_app_data(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+inline auto get_local_app_data(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     auto local_app_data = std::unique_ptr<char>{getenv("LOCALAPPDATA")};
     if (local_app_data != nullptr) {
-        auto const data_dir = std::filesystem::path{local_app_data.release()};
-        return std::optional<std::filesystem::path>{data_dir / name};
+        auto const data_dir = path{local_app_data.release()};
+        return optional<path>{data_dir / name};
     }
     return std::nullopt;
 }
@@ -70,12 +76,12 @@ namespace windows {
 
 namespace mac_os {
 
-[[nodiscard]] inline auto get_application_support(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+inline auto get_application_support(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     auto home = std::unique_ptr<char>{getenv("HOME")};
     if (home != nullptr) {
-        auto const home_dir = std::filesystem::path{home.release()};
-        return std::optional<std::filesystem::path>{home_dir / "Library" / "Application Support" / name};
+        auto const home_dir = path{home.release()};
+        return optional<path>{home_dir / "Library" / "Application Support" / name};
     }
     return std::nullopt;
 }
@@ -84,32 +90,32 @@ namespace mac_os {
 
 namespace xdg {
 
-[[nodiscard]] inline auto get_config_home(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+inline auto get_config_home(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     auto xdg_config_home = std::unique_ptr<char>{getenv("XDG_CONFIG_HOME")};
     if (xdg_config_home != nullptr) {
-        auto const config_dir = std::filesystem::path{xdg_config_home.release()};
-        return std::optional<std::filesystem::path>{config_dir / name};
+        auto const config_dir = path{xdg_config_home.release()};
+        return optional<path>{config_dir / name};
     }
     auto home = std::unique_ptr<char>{getenv("HOME")};
     if (home != nullptr) {
-        auto const home_dir = std::filesystem::path{home.release()};
-        return std::optional<std::filesystem::path>{home_dir / ".config" / name};
+        auto const home_dir = path{home.release()};
+        return optional<path>{home_dir / ".config" / name};
     }
     return std::nullopt;
 }
 
-[[nodiscard]] inline auto get_data_home(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+inline auto get_data_home(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     auto xdg_data_home = std::unique_ptr<char>{getenv("XDG_DATA_HOME")};
     if (xdg_data_home != nullptr) {
-        auto const data_dir = std::filesystem::path{xdg_data_home.release()};
-        return std::optional<std::filesystem::path>{data_dir / name};
+        auto const data_dir = path{xdg_data_home.release()};
+        return optional<path>{data_dir / name};
     }
     auto home = std::unique_ptr<char>{getenv("HOME")};
     if (home != nullptr) {
-        auto const home_dir = std::filesystem::path{home.release()};
-        return std::optional<std::filesystem::path>{home_dir / ".local" / "share" / name};
+        auto const home_dir = path{home.release()};
+        return optional<path>{home_dir / ".local" / "share" / name};
     }
     return std::nullopt;
 }
@@ -117,8 +123,8 @@ namespace xdg {
 } // namespace xdg
 
 template <Platform p = get_platform()>
-[[nodiscard]] auto get_config_dir(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+auto get_config_dir(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     if constexpr (p == Platform::Windows) {
         return windows::get_app_data(getenv, name);
     } else if constexpr (p == Platform::MacOS) {
@@ -129,8 +135,8 @@ template <Platform p = get_platform()>
 }
 
 template <Platform p = get_platform()>
-[[nodiscard]] auto get_data_dir(GetEnvFn getenv, std::string_view const name)
-    -> std::optional<std::filesystem::path> {
+[[nodiscard]]
+auto get_data_dir(GetEnvFn getenv, std::string_view const name) -> optional<path> {
     if constexpr (p == Platform::Windows) {
         return windows::get_local_app_data(getenv, name);
     } else if constexpr (p == Platform::MacOS) {
