f91e3629b60467b76842d4420057d6f33be70d7e
Henry Till <henrytill@gmail.com>
2025-10-10 22:01:33 -0700

    flake.nix: remove nixConfig

--- flake.nix
+++ flake.nix
@@ -4,13 +4,6 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  nixConfig = {
-    extra-substituters = [ "https://henrytill.cachix.org" ];
-    extra-trusted-public-keys = [
-      "henrytill.cachix.org-1:EOoUIk8e9627viyFmT6mfqghh/xtfnpzEtqT4jnyn1M="
-    ];
-  };
-
   outputs =
     {
       self,
